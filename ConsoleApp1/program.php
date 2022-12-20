69909b2ac741ec7573ebc76ac480e2ff1ca516f3
anniepyri <117916795+anniepyri@users.noreply.github.com>
2022-12-20 19:13:29 +0100

    Update program.php

--- ConsoleApp1/program.php
+++ ConsoleApp1/program.php
@@ -1,8 +1,8 @@
-﻿<?php
+<?php
 
-$baumhoehe = random_int(4, 6);
+$baumhoehe = random_int(4, 18);
 /* baumhoehe legt die Höhe zufällig, ohne Stamm und Stern, in einem Bereich von 3 bis 17, fest (der Bereich ist +1 angegeben,
- da später, beim erstellen der Äste, immer -1 gerechnet wird */
+ da später beim Erstellen der Äste, immer -1 gerechnet wird */
 
 $anzahlLeer = 5 + ($baumhoehe - 1);
 /* anzahlLeer bestimmt die Anzahl der Leerzeichen, die für die richtige Platzierung eines Elements benötigt werden.
@@ -59,33 +59,29 @@ function baum (int $baumhoehe) {
             $x = (2 * $j++) - 2; //Anzahl Sterne pro Reihe
             $y = 6 + ($i - 1); //Anzahl Leerzeichen pro Reihe, +6 für Randabstand
             $element = ''; //Variable, die das Innere des Baums beginnt, weitere Elemente werden rechts daran kokateniert
-            $array = [];
+            $array = []; 
 
 
             for ($k = 1; $k <= ($x+1); $k++) { //Schleife, für die Anzahl der Elemente pro Reihe
 
                 $z = random_int(1, 2); //generieren von Zufallszahl
-                $array[] = $z;
-
-                //var_dump($k-2);
-
+                $array[] = $z; //speichern der zufällig generierten Zahlen in einem Array
 
                 switch ($z) { //Auswahl zwischen Deko oder Nadel für jedes einzelne Element
                     case 1:
-                        $e=$k-2;
-                        //var_dump($e);
-                        if($e>=0){
-                            if($array[$e]===1) {
-                                //echo "bt";
-                                goto two;
+                        $e=$k-2; //zählt auf passenden Index für die Element Abfrage im Array
+                      
+                        if($e>=0) { //Bedingung, für alle Werte e Werte größer/gleich 0
+                            if($array[$e]===1) { // Bedingung, wenn das vorherige Element des Arrays 1 ist
+                                goto two; //wird zu case 2 gegangen
                             }
                         }
 
-                        $element .= $deko[random_int(0, $dekoCount)]; //zufällige Auswahl eines der drei Dekoelemente
+                        $element .= $deko[random_int(0, $dekoCount)]; //ansonsten zufällige Auswahl eines der drei Dekoelemente
                         break;
 
                     case 2;
-                        two:
+                        two: 
                         $element .= "\033[0;32m$nadel[0]\033[0m"; //Nadel wird Grün gefärbt
                         break;
                 }
@@ -93,11 +89,12 @@ function baum (int $baumhoehe) {
                 $baumAnfang = random_int(1,2); //generieren von Zufallszahl
 
                 switch ($baumAnfang){ //Auswahl für erstes Element einer Reihe zwischen Begrenzungsnadel oder Deko
-                    case 1: if($array[0]===1){
-                        $baumAnfang = "\033[0;32m/\033[0m"; //Nadel wird Grün gefärbt
-                    }else {
-                        $baumAnfang ="\033[0;36m*\033[0m"; //Deko wird Cyan gefärbt
-                    }
+                    case 1: 
+                        if($array[0]===1){
+                            $baumAnfang = "\033[0;32m/\033[0m"; //Nadel wird Grün gefärbt
+                        }else {
+                         $baumAnfang ="\033[0;36m*\033[0m"; //Deko wird Cyan gefärbt
+                        }
                         break;
 
                     case 2:
@@ -112,11 +109,12 @@ function baum (int $baumhoehe) {
                 $baumEnde = random_int(1,2); //generieren von Zufallszahl
 
                 switch ($baumEnde) { //Auswahl für letztes Element einer Reihe zwischen Begrenzungsnadel oder Deko
-                    case 1: if($array[($k-1)]===1){
-                        $baumEnde = "\033[0;32m\ \033[0m \n"; //Nadel wird Grün gefärbt
-                    }else {
-                        $baumEnde ="\033[0;36m*\033[0m \n"; //Deko wird Cyan gefärbt
-                    }
+                    case 1: 
+                        if($array[($k-1)]===1){
+                             $baumEnde = "\033[0;32m\ \033[0m \n"; //Nadel wird Grün gefärbt
+                        }else {
+                             $baumEnde ="\033[0;36m*\033[0m \n"; //Deko wird Cyan gefärbt
+                        }
                         break;
 
                     case 2:
@@ -133,24 +131,31 @@ function baum (int $baumhoehe) {
 
             $ast = $space . $baumAnfang . $element . $baumEnde; //erstellen eines Astes aus einzelnen Elementen
             echo $ast; //Ausgabe eines Astes
-
-           // print_r($array);
         }
     }
 }
 
 /* In der Funktion baum wird das Innere des Weihnachtsbaums generiert. Zunächst werden die Zählvariablen i und j
  mit Werten belegt. Dann werden zwei Arrays angelegt. Im Array nadel, ist die <<Nadel>> einer Tanne gespeichert.
- Das Array deko enthält drei verschiedene Dekoelemente, jeweils in einer anderen Farbe. In einer kopfgesteuerten while-
- Schleife, wird folgender Algorithmus solange ausgeführt, bis die baumhöhe erreicht ist. In einer weiteren while Schleife
- wird von der Baumhöhe jeweils eins abgezogen und der Algorithmus soange ausgeführt wie i größer 0 ist. Es werden die
- Anzahlen, der pro Reihe benötigten Anzahl an Elementen des Baums und der Leerzeichen für die Formatierung berechnet.
- In einer for- Schleife wird von 0 um eins hochgezählt bis die Anzahl an Elementen pro Reihe erreicht ist. Für jeden Durchlauf
- der Schleife, also für jedes Element eines Astes, wird eine Zufallszahl generiert, mithilfe derrer in einem Switch Case
- entschieden wird, ob das Element eine Nadel oder Deko ist. Nach dieser Schleife wird jeweils für das Erste und Letzte
+ Das Array deko enthält drei verschiedene Dekoelemente, jeweils in einer anderen Farbe. 
+ In einer kopfgesteuerten while-Schleife, wird folgender Algorithmus solange ausgeführt, bis die baumhöhe erreicht ist. 
+ In einer weiteren while Schleife wird von der Baumhöhe jeweils eins abgezogen und der Algorithmus solange ausgeführt wie i größer 0 ist. 
+ Es werden die Anzahlen, der pro Reihe benötigten Anzahl an Elementen des Baums und der Leerzeichen für die Formatierung berechnet.
+ 
+ In einer for- Schleife wird mit Startwert 0 pro Durchlauf +1 hochgezählt bis die Anzahl an Elementen pro Reihe erreicht ist. 
+ Für jeden Durchlauf der Schleife, also für jedes Element eines Astes, wird eine Zufallszahl generiert, mithilfe derrer in einem 
+ Switch Case entschieden wird, ob das Element eine Nadel oder Deko ist. Jede Zufallszahl für ein Element, wird in einem Array gespeichert. 
+ Im ersten Case wird mit einer if Bedingung überprüft, ob das vorherige Element des Arrays eine 1 (Deko) is, in diesem Fall wird 
+ in Case 2 gesprungen und eine Nadel statt einer weiteren Deko gesetzt. 
+
+ Nach dieser Schleife wird jeweils für das Erste und Letzte
  Element eines Astes eine Zufallszahl generiert, mithilfe derer in einem Switch Case entschieden wird, ob das jeweilige
- Element Deko oder eine Nadel ist. Eine weitere space Variabel stellt auch hier die Abstände zum Rand dar. Für stern und Stamm
- wird eine andere Berechnung verwendet als für das Innere des Baums. Zuletzt wird ein Ast aus dem passenden Leerabstand,
+ Element Deko oder eine Nadel ist.Auch hier wird mit einer If Bedingung überprüft ob das benachbarte Element Deko oder Nadel ist.
+ 
+ Eine weitere space Variabel stellt auch hier die Abstände zum Rand dar. Für stern und Stamm
+ wird eine andere Berechnung verwendet als für das Innere des Baums. 
+ 
+ Zuletzt wird ein Ast aus dem passenden Leerabstand,
  einem ersten Element, einer Element Reihe und einem letzten Element gebildet und ausgegeben. Durch den Schleifendurchlauf
  wird die entsprechende Anzahl an Ästen, die zu Beginn generiert wurde, erstellt. */
 
